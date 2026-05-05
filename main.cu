6e39fe3096744fb2f16da97a037a4cac9eeed502
z <z@zs-MacBook-Pro.local>
2026-05-05 12:08:36 -0700

    start

---
+++ main.cu
@@ -0,0 +1,27 @@
+#define N 512
+int main(void) {
+  int *a, *b, *c;
+  int *d_a, *d_b, *d_c;
+  int size = N * sizeof(int);
+  cudaMalloc((void **)&d_a, size);
+  cudaMalloc((void **)&d_b, size));
+  cudaMalloc((void **)&d_c, size));
+  a = (int *)malloc(size); random_ints(a, N);
+  b = (int *)malloc(size); random_ints(b, N);
+  c = (int *)malloc(size);
+  cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+  add<<N,1>>>(d_a, d_b, d_c);
+  add<<N,1>>>(d_a, d_b, d_c);
+  cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+  free(a); free(b); free(c);
+  cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+  return 0;
+}
+  
+__global__ void add(int *a, int *b, int *c) {
+  c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
+}
+
+
+
